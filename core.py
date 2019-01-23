47a5c6d412bb789515ceb5370e0fca8fdaad35d3
Serasar <you@example.com>
2019-01-23 11:15:01 +0300

    wrapped 2

--- core.py
+++ core.py
@@ -2,14 +2,17 @@ import hashlib
 import random
 import time
 
-def calculate_pow(challenge_string, difficulty)
-string = "Hsafgasdfsadfsafsfsdf"
-complete = False
-while complete == False:
-    n = ''.join(random.SystemRandom().choice('123456789') for i in range(10))
-    curr_string = string + str(n)
-    curr_hash = hashlib.sha512(bytes(curr_string,"utf-8")).hexdigest()
-    if curr_hash.startswith('000'):
-        print(curr_hash)
-        print(curr_string)
-        complete = True
+def calculate_pow(challenge_string, difficulty):
+    string = challenge_string
+    complete = False
+    while complete == False:
+        n = ''.join(random.SystemRandom().choice('123456789') for i in range(10))
+        curr_string = string + str(n)
+        curr_hash = hashlib.sha512(bytes(curr_string,"utf-8")).hexdigest()
+        if curr_hash.startswith(''.join('0' for i in range(difficulty))):
+            print(curr_hash)
+            print(curr_string)
+            complete = True
+    return curr_hash, curr_string
+
+#print(calculate_pow("test", 4))
